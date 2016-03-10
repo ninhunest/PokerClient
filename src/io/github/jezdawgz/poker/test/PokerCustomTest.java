14877181a499b2ddf834d4e4d749521f40f2aa07
jezdawgz <jeremy.collette@me.com>
2016-03-10 23:21:35 +1100

    (Backup)

--- src/io/github/jezdawgz/poker/test/PokerCustomTest.java
+++ src/io/github/jezdawgz/poker/test/PokerCustomTest.java
@@ -26,6 +26,8 @@ import io.github.jezdawgz.poker.server.Card.Suit;
 import io.github.jezdawgz.poker.server.Card.Value;
 import io.github.jezdawgz.poker.server.Hand;
 import io.github.jezdawgz.poker.server.HandAnalyser;
+import io.github.jezdawgz.poker.server.NewPot;
+import io.github.jezdawgz.poker.server.Player;
 
 
 /**
@@ -36,14 +38,35 @@ public class PokerCustomTest {
     
     public static void main(String[] args)
     {
+        /*
         Card[] cards = new Card[]{new Card(Suit.SPADES, Value.ACE), new Card(Suit.SPADES, Value.FIVE), new Card(Suit.CLUBS, Value.TWO), new Card(Suit.CLUBS, Value.THREE), new Card(Suit.CLUBS, Value.FOUR)};
         Hand h1 = HandAnalyser.getBestHand(null, new CardSet(cards), new CardSet(new Card[]{new Card(Suit.DIAMONDS, Value.ACE), new Card(Suit.HEARTS, Value.ACE)}));
         Hand h2 = HandAnalyser.getBestHand(null, new CardSet(cards), new CardSet(new Card[]{new Card(Suit.SPADES, Value.JACK), new Card(Suit.DIAMONDS, Value.JACK)}));
+        Hand h3 = HandAnalyser.getBestHand(null, new CardSet(cards), new CardSet(new Card[]{new Card(Card.Suit.DIAMONDS, Card.Value.SIX), new Card(Card.Suit.DIAMONDS, Card.Value.SEVEN)}));
+        Hand h4 = HandAnalyser.getBestHand(null, new CardSet(cards), new CardSet(new Card[]{new Card(Card.Suit.SPADES, Card.Value.SIX), new Card(Card.Suit.HEARTS, Card.Value.SEVEN)}));
+        
         System.out.println(h1.getType()+": "+h1.toString());
         System.out.println(h2.getType()+": "+h2.toString());
+        System.out.println(h3.getType()+": "+h3.toString());
+        System.out.println(h4.getType()+": "+h4.toString());        
         
         Hand.HandComparator comp = new Hand.HandComparator();
-        System.out.println(comp.compare(h1, h2));
+        System.out.println(comp.compare(h1, h4));
+        */
+        
+        Player[] players = new Player[]{new Player("Jeremy", null, null), new Player("Justin", null, null), new Player("Baker", null, null), new Player("Andy", null, null)};
+        int JEREMY = 0, JUSTIN = 1, BAKER = 2, ANDY = 3;
+        NewPot np = new NewPot(players);
+
+        np.setBet(players[JEREMY], 150);
+        np.setBet(players[JUSTIN], 150);
+        np.setBet(players[BAKER], 300);
+        np.setBet(players[ANDY], 400);
+        np.setBet(players[JEREMY], 200);
+        np.setBet(players[JUSTIN], 400);
+        np.setBet(players[BAKER], 350);
+        np.setBet(players[JUSTIN], 1000);
+        np.setBet(players[ANDY], 1000);
     }
     
 }
